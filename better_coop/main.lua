2c4d8d961978de2249b933e609f907736bd7899b
Brandon Mendrick <brandon@mendrickc.om>
2021-06-03 20:12:15 -0500

    Basic implementation of active item fix

--- better_coop/main.lua
+++ better_coop/main.lua
@@ -12,7 +12,7 @@ local starting_room_idx = 0
 -- Debug stuff
 local isDebugging = true
 
-function betterCoop:checkDebug(_betterCoop)
+function betterCoop.checkDebug(_betterCoop)
   if Game():GetLevel():GetStage() == LevelStage.STAGE1_1 and isDebugging then
     Isaac.Spawn(
       EntityType.ENTITY_PICKUP,
@@ -39,10 +39,10 @@ function betterCoop:checkDebug(_betterCoop)
   end
 end
 
-betterCoop:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, betterCoop:checkDebug)
+betterCoop:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, betterCoop.checkDebug)
 
 -- First room color spawning
-function betterCoop:onNewLevel(_betterCoop)
+function betterCoop.onNewLevel(_betterCoop)
   -- Only spawn on the first starting room
   if Game():GetLevel():GetStage() ~= LevelStage.STAGE1_1 then
     return nil
@@ -99,7 +99,7 @@ function betterCoop:onNewLevel(_betterCoop)
 
 end
 
-function betterCoop:onPreGetCollectible(_betterCoop)
+function betterCoop.onPreGetCollectible(_betterCoop)
   -- If we aren't in the first floor starting room, short circuit
   if Game():GetLevel():GetStage() == LevelStage.STAGE1_1 and
      Game():GetLevel():GetCurrentRoomIndex() ~= starting_room_idx then
@@ -113,5 +113,49 @@ function betterCoop:onPreGetCollectible(_betterCoop)
   return Isaac.GetItemIdByName(item_names[math.random(#item_names)])
 end
 
-betterCoop:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, betterCoop:onNewLevel)
-betterCoop:AddCallback(ModCallbacks.MC_PRE_GET_COLLECTIBLE, betterCoop:onPreGetCollectible)
+betterCoop:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, betterCoop.onNewLevel)
+betterCoop:AddCallback(ModCallbacks.MC_PRE_GET_COLLECTIBLE, betterCoop.onPreGetCollectible)
+
+-- Active Item checking
+local handledActiveItems = {}
+betterCoop:AddCallback(ModCallbacks.MC_POST_UPDATE, function(_betterCoop)
+  local room = Game():GetRoom()
+  -- Only care about boss and treasure rooms
+  if room:GetType() == RoomType.ROOM_TREASURE or room:GetType() == RoomType.ROOM_BOSS then
+    for _, entity in pairs(Isaac.GetRoomEntities()) do
+      -- Looking for non-empty collectible pedestals
+      if entity.Type == EntityType.ENTITY_PICKUP and entity.Variant == PickupVariant.PICKUP_COLLECTIBLE and entity.SubType != CollectibleType.COLLECTIBLE_NULL then
+        local item = ItemConfig():GetCollectible(entity.SubType)
+
+        if item.Type == ItemType.ITEM_ACTIVE and entity:ToPickup().Touched and not handledActiveItems[item.ID] then
+          -- Spawn an empty pedestal
+          Isaac.Spawn(
+            EntityType.ENTITY_PICKUP,
+            PickupVariant.PICKUP_COLLECTIBLE,
+            CollectibleType.COLLECTIBLE_NULL,
+            entity.Position
+            Vector(0,0),
+            nil
+          )
+
+          -- Make sure we don't spawn more pedestals than needed
+          handledActiveItems[item.ID] = true
+          for i = 0, Game():GetNumPlayers() do
+            playerPrimaryActive = Isaac.GetPlayer(i):GetActiveItem()
+            playerSecondaryActive = Isaac.GetPlayer(i):GetActiveItem(ActiveSlot.SLOT_SECONDARY)
+
+            if playerPrimaryActive then
+              handledActiveItems[playerPrimaryActive] = true
+            end
+
+            if playerSecondaryActive then
+              handledActiveItems[playerSecondaryActive] = true
+            end
+          end
+
+          break
+        end
+      end
+    end
+  end
+end)
